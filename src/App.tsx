3998d30b4bf335dc8acac6b45585472ee182d581
warmbit <warmbit1@gmail.com>
2020-05-20 10:39:10 -0600

    hello

---
+++ src/App.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+interface AppProps {
+  name: string
+}
+
+const App: React.FC<AppProps> = ({name}) => {
+  return (
+    <div className="App">
+      Hello, {name}!
+    </div>
+  );
+}
+
+/*
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+*/
+
+export default App;
